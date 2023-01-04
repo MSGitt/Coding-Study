9de4f92f88d85f803fcab7e1a74e6dcda56ec03e
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-04 16:22:18 +0900

    [Gold IV] Title: 최단경로, Time: 652 ms, Memory: 68220 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,44 @@
+import sys
+import heapq
+
+v, e = map(int, sys.stdin.readline().split())
+k = int(sys.stdin.readline())
+
+matrix = [[] * (v + 1) for i in range(v + 1)]
+visited = [float('inf')] * (v + 1)
+
+for i in range(e) :
+    
+    a, b, c = map(int, sys.stdin.readline().split())
+    matrix[a].append((b, c))
+    
+def find(x, matrix, visited) :
+    
+    queue = []
+    
+    visited[x] = 0
+    heapq.heappush(queue, (0, x))
+    
+    while queue :
+        w, p = heapq.heappop(queue)
+               
+        if visited[p] < w :
+            continue
+            
+        for new_p, new_w in matrix[p] : 
+            weight = w + new_w
+            if weight < visited[new_p] :
+                visited[new_p] = weight
+                heapq.heappush(queue, (weight, new_p))
+                                                   
+    return visited 
+
+result = find(k, matrix, visited) 
+
+for i in range(1, len(result)) :
+    if result[i] == float('inf') :
+        print('INF')
+
+    else :
+        print(result[i])
+                                   
