daae6e8317903851840c6a98ba93cceac8fe9379
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-11 00:41:31 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+   
+
+    numbers = [str(i) for i in numbers]
+    
+    
+    numbers.sort(key = lambda x : x * 3, reverse = True)
+    
+    return str(int(''.join(numbers)))
