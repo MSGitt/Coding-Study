93febb78c8e8f7fcbe8992e9f42bd3efd9b010fb
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-10 12:05:09 +0900

    [level 3] Title: 거스름돈, Time: 431.30 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,16 @@
+def solution(n, money):
+    
+    money.sort()
+    
+    answer = 0
+    
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    
+    for i in money :
+        for j in range(i, n + 1) :
+            if j >= i :
+                dp[j] += dp[j - i]
+                
+    return dp[n] % 1000000007
+    
