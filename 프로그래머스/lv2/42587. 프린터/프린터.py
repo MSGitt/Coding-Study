d5ef47f157b2484db75484cf748b19a747faff31
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-12 10:51:53 +0900

    [level 2] Title: 프린터, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -0,0 +1,22 @@
+def solution(priorities, location):
+    
+    stack = []
+    
+    
+    queue = [(i, p) for i, p in enumerate(priorities)]
+    
+    m = max(priorities) 
+    result = 0
+    
+    while True :
+        k = queue.pop(0)
+        
+        if k[1] != max(priorities) :
+            queue.append(k)
+            
+        else : 
+            result += 1 
+            priorities.remove(k[1])
+            
+            if k[0] == location :
+                return result
