4e872e231d54caabd2f71e550cc1ba935491abc6
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-12 23:08:31 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,18 +1,26 @@
+import math
+
 def solution(brown, yellow):
     
-    total = brown + yellow 
-    
-    i = 3
-    
-    while True :
-        
-        if total % i == 0 :
-            row = total // i
-            col = i 
+         
+    for i in range(1, int(math.sqrt(yellow)) + 1) : 
+        if yellow % i == 0 :
+            if 2 * (yellow // i + i) == brown - 4 :
+                return [yellow//i + 2, i + 2]
             
-            if (row - 2) * (col - 2) == yellow :
-                return [row, col]
+#     total = brown + yellow 
+    
+#     i = 3
+    
+#     while True :
         
-        i += 1
+#         if total % i == 0 :
+#             row = total // i
+#             col = i 
+            
+#             if (row - 2) * (col - 2) == yellow :
+#                 return [row, col]
+        
+#         i += 1
         
     
