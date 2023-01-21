64af21018b698ee119a70b3218f65cea469ff507
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-21 17:00:51 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,30 @@
 import sys
 from collections import deque
 
+m, n = map(int, sys.stdin.readline().split())
 
-n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().rstrip())) for i in range(m)]
 
-matrix = [list(map(int, sys.stdin.readline().rstrip())) for i in range(n)]
-visited = [[0] * m for i in range(n) ]
-
-def bfs(x, y, matrix, visited, n, m) :
-    
-    row = [1, -1, 0, 0]
-    col = [0, 0, 1, -1]
+def bfs(x, y) :
     
     queue = deque()
-    
+    row = [1, -1, 0, 0]
+    col = [0, 0, 1, -1]
+        
     queue.append((x, y))
-    visited[x][y] = 1
+    graph[x][y] = 2
     
     while queue :
         a, b = queue.popleft()
         for i in range(4) :
             newx = a + row[i]
             newy = b + col[i]
-            if 0 <= newx < n and 0 <= newy < m and matrix[newx][newy] == 1 and visited[newx][newy] == 0:
+            if 0 <= newx < m and 0 <= newy < n and graph[newx][newy] == 1 :
+                graph[newx][newy] = graph[a][b] + 1
                 queue.append((newx, newy))
-                visited[newx][newy] = 1
-                matrix[newx][newy] = matrix[a][b] + 1 
                 
-bfs(0, 0, matrix, visited, n, m)
+    return graph[-1][-1] - 1
 
-print(matrix[n-1][m-1])
+result = bfs(0, 0)
 
-
-            
-        
+print(result)
