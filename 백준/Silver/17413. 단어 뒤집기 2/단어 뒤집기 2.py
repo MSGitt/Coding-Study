b72308f9a0b46bd092d1a3c95eae62cdae89bb94
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-22 19:52:31 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 68 ms, Memory: 32824 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,27 @@
+import sys
+
+word = list(sys.stdin.readline().rstrip())
+
+i, start = 0, 0
+
+while i < len(word) :
+    
+    if word[i] == '<' :
+        i += 1
+        while word[i] != '>' :
+            i += 1
+            
+        i += 1
+        
+    elif word[i].isalnum() :
+        start = i
+        
+        while i < len(word) and word[i].isalnum() :
+            i += 1
+            
+        word[start: i] = word[start: i][::-1]
+        
+    else :
+        i += 1
+        
+print(''.join(word))
