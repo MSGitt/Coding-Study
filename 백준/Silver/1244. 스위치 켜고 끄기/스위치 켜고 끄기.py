cb30b4f18ce48078d03767d2d385a01282aae484
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-22 12:48:37 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,46 +1,41 @@
-import sys 
+import sys
 
+n = int(sys.stdin.readline())
+switch = list(map(int, sys.stdin.readline().split()))
 
-n = int(sys.stdin.readline()) 
-num = list(map(int, sys.stdin.readline().split())) 
-stu = int(sys.stdin.readline()) 
+m = int(sys.stdin.readline())
 
-
-for i in range(stu) :
+for i in range(m) :
     a, b = map(int, sys.stdin.readline().split())
     
     if a == 1 :
-        for i in range(len(num)) :
-            if (i+1) % b == 0 :
-                num[i] = int(not num[i])
+        for j in range(len(switch)) :
+            if (j + 1) % b == 0 :
+                switch[j] = abs(switch[j] - 1)
                 
-    else :
-        for j in range(len(num)) :
-            if (j+1) == b :
-                front = j - 1 
-                back = j + 1
-                num[j] = int(not num[j]) 
+    if a == 2 :
+        for j in range(len(switch)) :
+            if (j + 1) == b :
+                left = j - 1
+                right = j + 1
+                switch[j] = abs(switch[j] - 1)
                 
-                while True : 
-                    if front >=0 and back <= len(num) - 1   :
-                        if num[front] == num[back] :
-                            num[front    ] = int(not num[front])
-                            num[back] = int(not num[back])
-                            
-                        else : 
-                            break 
+                while left >= 0 and right < len(switch) :
+                    if switch[left] == switch[right] :
+                        switch[left] = abs(switch[left] - 1)
+                        switch[right] = abs(switch[right] - 1)
                         
-                    else : 
+                    else :
                         break
+                        
+                    left -= 1
+                    right += 1
                     
-                    front -= 1
-                    back += 1 
-            
 k = 0
-while k < len(num) : 
-    print(num[k], end = " ") 
+
+while k < len(switch) : 
+    print(switch[k], end = " ") 
     if k % 20 == 19 : 
         print()
         
-    k += 1 
-        
+    k += 1     
