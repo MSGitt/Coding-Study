cf1cf7fef0da50984db45f3e3077c283ca7d76f0
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-23 19:11:48 +0900

    [Gold V] Title: 퇴사 2, Time: 800 ms, Memory: 174892 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -12,8 +12,7 @@ for i in range(1, n + 1) :
     
 for i in range(1, n + 1) :
     
-    dp[i] = max(dp[i], dp[i-1])
-    
+    dp[i] = max(dp[i], dp[i - 1])
     finish = i + number[i][0] - 1
     
     if finish <= n :
