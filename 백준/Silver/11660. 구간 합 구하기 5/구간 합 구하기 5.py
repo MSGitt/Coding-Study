5d6faabbc1ff5bbdd2c771dc731cfa844e9f0fa7
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-23 15:24:01 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1048 ms, Memory: 105996 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -7,10 +7,10 @@ dp = [[0]*(n + 1) for i in range(n+1)]
 
 for i in range(1, n + 1) :
     for j in range(1, n + 1) :
-        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + number[i-1][j-1]
-
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + number[i - 1][j - 1]
         
 for i in range(m) :
     
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
-    print(dp[x2][y2] - dp[x2][y1-1]- dp[x1-1][y2] + dp[x1-1][y1-1])
+    
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])          
