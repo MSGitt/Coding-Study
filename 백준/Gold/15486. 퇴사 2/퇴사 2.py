6021ed0c86ca4f00286012b5e59cdae9f02f79f1
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-23 17:59:42 +0900

    [Gold V] Title: 퇴사 2, Time: 2812 ms, Memory: 248416 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+number = [0] * (n + 1)
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1) :
+    
+    a, b = map(int, sys.stdin.readline().split())
+    number[i] = (a, b)
+    
+for i in range(1, n + 1) :
+    
+    dp[i] = max(dp[i], dp[i-1])
+    
+    finish = i + number[i][0] - 1
+    
+    if finish <= n :
+        dp[finish] = max(dp[finish], dp[i - 1] + number[i][1])
+        
+print(max(dp))
