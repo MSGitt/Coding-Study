669be1f8c8b5a16cf137b06044a06b8070ba8dd8
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-24 18:54:49 +0900

    [level 3] Title: 단속카메라, Time: 0.92 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,12 @@
+def solution(routes):
+    answer = 0
+    
+    routes.sort(key = lambda x : x[1])
+    cam = -30001
+    
+    for i in routes : 
+        if cam < i[0] :
+            answer += 1
+            cam = i[1]
+    
+    return answer
