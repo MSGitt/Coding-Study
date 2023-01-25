5e482fd8d5590697405b72c10cc21f2449f425bf
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-01-26 02:04:55 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 168 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -3,13 +3,11 @@ import sys
 n = int(sys.stdin.readline())
 number = list(map(int, sys.stdin.readline().split()))
 
-dp = [0] * n
+dp = number[:]
 
-for i in range(n) :
-    for j in range(n) :
-        if number[i] > number[j] and dp[i] < dp[j] :
-            dp[i] = dp[j]
-            
-    dp[i] += number[i]
+for i in range(1, n) :
+    for j in range(i) :
+        if number[i] > number[j] :
+            dp[i] = max(dp[i], dp[j] + number[i])
     
 print(max(dp))
